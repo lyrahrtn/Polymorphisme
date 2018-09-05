cb2ffd02243af2ea16c4c2db5a43341e49f5faaa
SMK TELKOM <SMK TELKOM@LYRA>
2018-09-05 13:52:45 -0700

    Commit keseluruhan

---
+++ src/TugasPraktikum/operasi.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasPraktikum;
+
+/**
+ *
+ * @author SMK TELKOM
+ */
+public interface operasi {
+    public double penjumlahan();
+    public double pengurangan ();
+    public double perkalian();
+    public double pembagian();
+}
